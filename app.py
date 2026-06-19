0bb6d2c64cd1ab1442d8740c264103ac34804798
AjinkyaPagare <pagareajinkya24@gmail.com>
2026-06-19 18:36:46 +0530

    Allow all CORS origins for socket.io

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'u19-secure-military-grade'
-socketio = SocketIO(app, cors_allowed_origins=["https://u19sender.pages.dev"], async_mode='eventlet')
+socketio = SocketIO(app, cors_allowed_origins="*", async_mode='eventlet')
 
 rooms = {}
 
